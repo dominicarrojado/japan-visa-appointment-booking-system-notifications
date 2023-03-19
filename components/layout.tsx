c53fa3954d16b9e8a87c647dd65604e2f0a79b65
Dominic Arrojado <dominicarrojado@gmail.com>
2023-03-19 22:01:18 +0800

    set up tailwindcss

---
+++ components/layout.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+  return <main>{children}</main>;
+}
