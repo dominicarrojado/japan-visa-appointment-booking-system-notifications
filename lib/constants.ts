7dba0759bd10ae94278bfd4f7d525dead3d3605f
Dominic Arrojado <dominicarrojado@gmail.com>
2023-03-21 17:21:02 +0800

    update meta description

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,7 +3,8 @@ export const PROJECT_TITLE =
   'Embassy of Japan in Singapore Visa Appointment Notification Service for Visa (Tourism) applications';
 export const MAIN_AUTHOR = 'Dominic Arrojado';
 export const MAIN_TITLE = `${PROJECT_TITLE} | ${SITE_NAME}`;
-export const MAIN_DESC = '';
+export const MAIN_DESC =
+  "My web application provides a convenient and cost-effective solution for those looking to apply for a tourist visa to Japan through the Embassy of Japan in Singapore. With this service, you will receive timely notifications when a visa appointment slot becomes available, saving you time and money. Apply for your visa with ease and confidence knowing that you won't miss your chance to secure an appointment. Sign up today!";
 export const MAIN_ORIGIN = 'https://dominicarrojado.com';
 export const MAIN_PATH =
   '/japan-visa-appointment-booking-system-notifications/';
